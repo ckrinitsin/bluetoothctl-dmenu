08be832de68821c51fcd7456095b15c51a311d9a
noahpy <noah.schlenker2002@gmail.com>
2024-04-21 14:33:56 +0200

    review correction

--- bluetooth-devices.sh
+++ bluetooth-devices.sh
@@ -1,7 +1,30 @@
 #
 # This program lists all paired devices which you can select from, the selected one
-# will be connected
+# will be connected / disconnected 
 #   
+
+connect_bluetooth(){
+    local MAC=$1
+    local DEVICE=$2
+    if bluetoothctl connect $MAC | grep -q 'successful' 
+    then
+        notify-send -t 5000 -r 2954 -u normal "  Connected successfully from" "     $DEVICE"
+    else 
+        notify-send -t 5000 -r 2954 -u normal "  Couldn't connect from" "     $DEVICE"
+    fi
+}
+
+
+disconnect_bluetooth(){
+    local MAC=$1
+    local DEVICE=$2
+    if bluetoothctl disconnect $MAC | grep -q 'successful' 
+    then
+        notify-send -t 5000 -r 2954 -u normal "  Disconnected successfully from" "     $DEVICE"
+    else 
+        notify-send -t 5000 -r 2954 -u normal "  Couldn't disconnect from" "     $DEVICE"
+    fi
+}
 
 # Opens dmenu prompt, which lets you decide which device you want to connect to
 DEVICE=$(bluetoothctl devices | sed 's/[^ ]* //' | sed 's/[^ ]* //' | dmenu -i)
@@ -14,22 +37,13 @@ fi
 # Get MAC adress of the device you selected
 MAC=$(bluetoothctl devices | grep "$DEVICE" | sed 's/[^ ]* //' | cut -d ' ' -f1)
 
-# If bluetooth device is already connected, disconnect
+# If bluetooth device is already connected, disconnect, else connect
 CONNECTED=$(bluetoothctl devices Connected | cut -f3 -d ' ')
-if echo $CONNECTED | grep $DEVICE; then
-    if bluetoothctl disconnect $MAC | grep -q 'successful' 
-    then
-        notify-send -t 5000 -r 2954 -u normal "  Disconnected successfully from" "     $DEVICE"
-    else 
-        notify-send -t 5000 -r 2954 -u normal "  Couldn't disconnect from" "     $DEVICE"
-    fi 
-    exit 0
+if echo $CONNECTED | grep $DEVICE
+then
+    disconnect_bluetooth $MAC $DEVICE
+else 
+    connect_bluetooth $MAC $DEVICE
 fi
 
-# Send a notify whether the connection was successful 
-if bluetoothctl connect $MAC | grep -q 'successful' 
-then
-    notify-send -t 5000 -r 2954 -u normal "  Connected successfully to" "     $DEVICE"
-else 
-    notify-send -t 5000 -r 2954 -u normal "  Couldn't connect to" "     $DEVICE"
-fi
+
